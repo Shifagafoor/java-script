1cc9adb133f04ba0f2611dfa9b960184166bb252
shifa gafoor <shifagafoorncas@gmail.com>
2024-11-13 15:00:36 +0530

    .

--- chapter1/vowels.js
+++ chapter1/vowels.js
@@ -1,5 +1,13 @@
 // Check if the given alphabet is vowel or consonant.
+function alphabetInVowel(s) {
+   let vowels = ["a","e","i","o","u"];
 
-function alphabetInVowel() {
-    vowels = [a , e , i , o , u]
+    if(vowels.includes(s)){
+        return("the given value is vowel")
+    }else{
+        return("the given value is not vowel")
+    }
+
 }
+
+console.log(alphabetInVowel("i"));
