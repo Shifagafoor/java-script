f07b8c5b1e23decda1a9f3da211ee678d1b69821
Shifa Gafoor A <shifagafoorncas@gmail.com>
2024-11-21 08:50:33 +0530

    ,

--- chapter2/qp05.js
+++ chapter2/qp05.js
@@ -1,2 +1,28 @@
-// Write a program to check if the given matrix is a diagonal matrix
+function diagonalMatrix(matrix){
+    const n = matrix.length;
 
+    for( let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++) {
+            if(i !== j && matrix[i][j] !== 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+const matrix1 = [
+    [1, 0, 0],
+    [0, 2, 0],
+    [0, 0, 3]
+];
+
+const matrix2 = [
+    [1, 0, 0],
+    [0, 2, 3],
+    [0, 0, 3]
+];
+
+console.log(diagonalMatrix(matrix1));
+console.log(diagonalMatrix(matrix2));
