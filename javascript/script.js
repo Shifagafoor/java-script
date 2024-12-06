c7e496c9e3ae8d1bfa8ca28ff5adf36f7731bcb3
Shifa Gafoor A <shifagafoorncas@gmail.com>
2024-12-06 13:39:45 +0530

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,48 @@
 //logical operators
 
 
+// let a = 1;
+// let b = 1;
+
+// console.log(a > 2 && a > 2)
+
+// &&
+// true true = true
+// true false = false
+// false false = false
+
+// let a = 10;
+// let b = 10;
+
+// console.log(a>2 || b>2);
+
+// ||
+
+// true true = true
+// false false = false
+// true false = true
+
+// let a = 5;
+// let b = 8;
+// let value = false
+// console.log(!value)
+
+// !value
+// if te value is true display false
+// if the value is false display true
+
+
+
+//conditional operators
+
+// let x = 5;
+// let y = x === 5 ? 10 : 6;
+
+// console.log(y)
+
+
+function myFunction() {
+    let a = 5
+    console.log(a)
+}
+myFunction()
